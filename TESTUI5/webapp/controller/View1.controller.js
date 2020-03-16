7ed33ffd2a7fb2e5494eed919e5c54d4ae851373
chunghan477 <chunghan477@naver.com>
2020-03-16 10:39:02 +0000

    TEST02

---
+++ TESTUI5/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("TESTUI5.TESTUI5.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ TESTUI5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"TESTUI5/TESTUI5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
